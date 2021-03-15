f21fe399b42d49b0234f1f51a786c76525b87941
DESKTOP-U04FTGO\User <ahmad.khrezaty@gmail.com>
2021-03-15 19:06:53 +0200

    Delete all related with branch when delete it

--- application/controllers/Branch.php
+++ application/controllers/Branch.php
@@ -90,6 +90,268 @@ class Branch extends Admin_Controller
         if (is_superadmin_loggedin()) {
             $this->db->where('id', $id);
             $this->db->delete('branch');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('accounts');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('advance_salary');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('attachments');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('attachments_type');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('award');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('book');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('book_category');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('award');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('book_issues');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('bulk_msg_category');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('bulk_sms_email');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('card_templete');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('	certificates_templete');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('class');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('custom_field');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('email_config');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('email_templates_details');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('enroll');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('event');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('event_types');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('exam');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('exam_attendance');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('exam_hall');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('exam_mark_distribution');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('exam_term');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('fees_reminder');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('fees_type');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('fee_allocation');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('fee_fine');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('fee_groups');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_about');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_admission');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_contact');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('	front_cms_events');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_faq');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_faq_list');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_home');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_home_seo');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_menu');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_pages');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_services');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_services_list');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_setting');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_teachers');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('front_cms_testimonial');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('grade');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('hall_allocation');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('homework');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('hostel');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('hostel_category');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('hostel_room');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('leave_application');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('leave_category');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('live_class');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('live_class_config');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('mark');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('online_admission');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('parent');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('payment_config');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('payment_types');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('payslip');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('salary_template');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('section');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('	semysms_config');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('	staff');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('staff_attendance');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('staff_department');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('staff_designation');
+
+            $this->db->where('branch_id', $id);
+            $student_category_id = '';
+            $student_category = $this->db->get('student_category')->row_array();
+            if($student_category_id != '')
+                $student_category_id = $student_category['id'];
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('student_category');
+
+            $this->db->where('category_id', $student_category_id);
+            $this->db->delete('student');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('student_attendance');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('student_category');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('subject');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('subject_assign');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('teacher_allocation');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('teacher_note');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('timetable_class');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('timetable_exam');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('transactions');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('transactions_links');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('transport_assign');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('transport_route');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('transport_stoppage');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('transport_vehicle');
+
+            $this->db->where('branch_id', $id);
+            $this->db->delete('voucher_head');
+
         } else {
             redirect(base_url(), 'refresh');
         }
